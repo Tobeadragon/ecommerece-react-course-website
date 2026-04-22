1c75773523e7345e3f551610ca45245a94a6a2d1
Tobeadragon <mastertyj@hotmail.com>
2026-04-22 22:51:57 +0200

    complete simple ecommerece site

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,20 +3,19 @@ import { getProducts } from "../data/products";
 
 export default function Home() {
   const products = getProducts();
-
   return (
     <div className="page">
       <div className="home-hero">
-        <div className="home-title">Welcome to ShopHub</div>
+        <h1 className="home-title">Welcome to ShopHub</h1>
         <p className="home-subtitle">
-          Discover amazing Products at great prices
+          Discover amazing products at great prices
         </p>
       </div>
       <div className="container">
         <h2 className="page-title">Our Products</h2>
         <div className="product-grid">
           {products.map((product) => (
-            <ProductCard product={product} key={product.id}/>
+            <ProductCard product={product} key={product.id} />
           ))}
         </div>
       </div>
